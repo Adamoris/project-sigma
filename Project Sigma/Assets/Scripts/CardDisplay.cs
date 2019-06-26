7b39ba9c721e75a146c88aa9cda72477514863b1
Unknown <joohyung122002@gmail.com>
2019-06-26 11:52:56 -0700

    Updated packages + installed cinemachine
    Some minor edits to the card scriptable object as well.

--- Project Sigma/Assets/Scripts/CardDisplay.cs
+++ Project Sigma/Assets/Scripts/CardDisplay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CardDisplay : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class CardDisplay : MonoBehaviour
     public Image artworkImage;
 
     public Text nameText;
+    public Text hpText;
+    public Text atkText;
+    public Text spdText;
+    public Text defText;
+    public Text resText;
+    public TextMeshProUGUI newName;
+
 
     // Start is called before the first frame update
     void Start()

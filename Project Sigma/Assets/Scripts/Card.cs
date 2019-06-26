66d6c8fb4e85d7d046d230a7115e2e19f3393349
Unknown <joohyung122002@gmail.com>
2019-06-26 01:33:14 -0700

    Updated Engine + more work on scriptable objects
    Current engine version: 2019.1.8f1
    
    Work on scriptable objects has progressed in addition to the individual details of the cards becoming more modular.

---
+++ Project Sigma/Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public Sprite artwork;
+
+    public new string name;
+    public string merge;
+
+    public string affinity;
+
+    public int HP;
+    public int Atk;
+    public int Spd;
+    public int Def;
+    public int Res;
+}
+

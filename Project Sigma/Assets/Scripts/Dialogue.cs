c382a13397f90fc68e38c9e7592fa8552ccbef25
Unknown <joohyung122002@gmail.com>
2019-07-07 08:30:44 -0700

    Created and worked on a dialogue scene.
    Some UI assets were also updated into sprite/UI elements.

---
+++ Project Sigma/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed = 0.02f;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index] && Input.GetKeyDown("Space")) ;
+        {
+            NextSentence();
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+
+}

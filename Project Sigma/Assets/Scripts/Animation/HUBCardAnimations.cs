a7b88fd32cfbd619b61a9fcad8bfdc5165e332e4
Unknown <joohyung122002@gmail.com>
2019-07-15 22:25:12 -0700

    UI animation work started + more assets imported
    Card assets were imported to better improve the UI concepts. Animation for certain elements have begun. The foundations for a more through modular unit system is prepared.

---
+++ Project Sigma/Assets/Scripts/Animation/HUBCardAnimations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUBCardAnimations : MonoBehaviour
+{
+    Animator card_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        card_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            card_animator.Play("Card Shift Up");
+        }
+    }
+}

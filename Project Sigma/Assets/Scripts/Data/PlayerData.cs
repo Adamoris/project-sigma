fe6b6d4bd2d02d76bbecba852459d66e850941ca
Unknown <joohyung122002@gmail.com>
2019-07-16 20:58:21 -0700

    Mouse Cursor + Save Functionality
    More assets were imported to test the custom mouse cursor and created a rudimentary save system using binary files.

--- Project Sigma/Assets/Scripts/Data/PlayerData.cs
+++ Project Sigma/Assets/Scripts/Data/PlayerData.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerData
 {
+    public int playerLevel;
+    public int premiumCurrencyCount;
 
+    public PlayerData (PlayerProgress progress)
+    {
+        playerLevel = progress.playerLevel;
+        premiumCurrencyCount = progress.premiumCurrencyCount;
+    }
 }
---
+++ Project Sigma/Assets/Scripts/Data/PlayerProgress.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerProgress : MonoBehaviour
+{
+    public int playerLevel = 1;
+    public int premiumCurrencyCount;
+
+    
+
+    public void SaveProgress()
+    {
+        SaveSystem.SaveProgress(this);
+    }
+
+    public void LoadProgress()
+    {
+        PlayerData data = SaveSystem.LoadProgress();
+
+        playerLevel = data.playerLevel;
+        premiumCurrencyCount = data.premiumCurrencyCount;
+    }
+
+    #region testing
+
+    public TextMeshProUGUI levelCounter;
+    public TextMeshProUGUI premiumCurrencyCounter;
+
+    public void Update()
+    {
+        levelCounter.text = "" + playerLevel;
+        premiumCurrencyCounter.text = "" + premiumCurrencyCount;
+
+    }
+
+    public void ChangeLevel(int amount)
+    {
+        playerLevel += amount;
+    }
+
+    public void ChangeCurrency(int amount)
+    {
+        premiumCurrencyCount += amount;
+    }
+
+    #endregion
+
+}

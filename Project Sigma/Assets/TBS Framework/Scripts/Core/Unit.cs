0607fc853fb7fbb09a98268d604ec4bcc00d335b
Unknown <joohyung122002@gmail.com>
2019-09-22 19:48:06 -0700

    DC/CC Weapon functionality implemented
    Weapons that have the DC/CC functionality enabled also work in combat.

--- Project Sigma/Assets/TBS Framework/Scripts/Core/Unit.cs
+++ Project Sigma/Assets/TBS Framework/Scripts/Core/Unit.cs
@@ -280,8 +280,8 @@ public abstract class Unit : MonoBehaviour
 
         MarkAsAttacking(other);
         ActionPoints--;
-        if (card.range == other.card.range || (card.range == Card.Range.Melee && other.card.slotA.counter == Equipment.Counter.Melee)
-            || (card.range == Card.Range.Ranged && other.card.slotA.counter == Equipment.Counter.Ranged))
+        if (card.range == other.card.range || (card.range == Card.Range.Melee && (other.card.slotA.counter == Equipment.Counter.Melee || other.card.weapon.CloseCounter))
+            || (card.range == Card.Range.Ranged && (other.card.slotA.counter == Equipment.Counter.Ranged || other.card.weapon.DistantCounter)))
         {
             other.CounterPoints++;
         }
